914f11cadd7a5c2957cbbb47295e0d94d9342ea2
CredoM <942794848@qq.com>
2018-07-22 15:34:23 +0800

    修改配置信息

--- build/utils.js
+++ build/utils.js
@@ -18,8 +18,16 @@ exports.cssLoaders = function (options) {
   const cssLoader = {
     loader: 'css-loader',
     options: {
-      sourceMap: options.sourceMap
+      sourceMap: options.sourceMap,
+      importLoaders: 2
     }
+  }
+
+  const px2remLoader = {
+      loader: 'px2rem-loader',
+      options: {
+          remUnit: 75
+      }
   }
 
   const postcssLoader = {
@@ -47,7 +55,8 @@ exports.cssLoaders = function (options) {
     if (options.extract) {
       return ExtractTextPlugin.extract({
         use: loaders,
-        fallback: 'vue-style-loader'
+        fallback: 'vue-style-loader',
+        publicPath: '../../'
       })
     } else {
       return ['vue-style-loader'].concat(loaders)
